b4730c00c1971aa42bb5521e0e6bf9d6adbf115e
mkolesnyova <92516020+mkolesnyova@users.noreply.github.com>
2022-03-14 18:34:19 +0200

    Add files via upload

---
+++ lab12(2).cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>;
+#include <string.h>;
+#include <cctype>; // isdigit
+using namespace std;
+void ff(char ss[]);
+void ff(char ss[])
+{
+	for (int i = 0; i < 200; i++) 
+		if (isdigit(ss[i]))
+	{
+		cout << i + 1;
+		cout << endl;
+		break;
+	}
+};
+int main()
+{
+	char s1[40] = "Hello, I'm Kate. I'm 14.";
+	cout << "1st figure position in s1 : ";
+	ff(s1);
+	char s2[70] = "My name's Susi, I'm a doctor. My age is 34.";
+	cout << "1st figure position in s2 : ";
+	ff(s2);
+}
